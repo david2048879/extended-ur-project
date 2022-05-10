4c8a1a2adfdded57c46e4494aeae42ff97d1830b
Ngendahimana David <91059058+david2048879@users.noreply.github.com>
2022-05-10 10:56:37 +0200

    Add files via upload
    initial commit

---
+++ js.js
@@ -0,0 +1,67 @@
+// const menuToggle = document.querySelector('.toggle');
+// const showcase = document.querySelector('.showcase');
+
+// menuToggle.addEventListener('click', () => {
+//   menuToggle.classList.toggle('active');
+//   showcase.classList.toggle('active');
+// })
+
+
+
+
+/* Owl Carousel 2 All Settings. See the bottom for how to use equal heights with matchHeight  plugin */
+
+$(document).ready(function() {
+ 
+
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+        $(".nav-bar").addClass("sticky");
+    }else{
+        $(".nav-bar").removeClass("sticky");
+    }
+});
+  $(".owl-carousel").owlCarousel();
+
+  $('.carousel').owlCarousel({
+    margin: 50,
+    loop: true,
+    autoplay:true,
+    autoPlayTimeOut:500,
+    autoplaySpeed: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+        },
+        600:{
+            items: 2,
+        },
+        1000:{
+            items: 3,
+        },
+    }
+  });
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.from(".col-content", {
+    duration:3,
+    x:100,
+    scrollTrigger:{
+      trigger:".project-left",
+      toggleActions: "restart reverse resume complete"
+    }
+  })
+
+  gsap.from(".col-img", {
+    duration:3,
+    opacity:0,
+    scrollTrigger:{
+      trigger:".project-left",
+      toggleActions: "restart reverse resume complete"
+    }
+  })
+
+ 
+});
